68ca078c4d51c240adc95e08640120bd6a323272
FredZenobius <joytoy390@gmail.com>
2023-03-07 20:58:53 +0900

    10951.py

---
+++ python/10951.py
@@ -0,0 +1,10 @@
+number_list = []
+a = []
+while True:
+    try :
+        a = list(map(int,input().split())) 
+    except Exception as e :
+        break
+# print(number_list)
+for i in range(1, int(len(number_list)/2+1)) :
+    print(number_list[i*2-1] + number_list[i*2-2])
